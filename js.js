4ad634758ca595482680026543b18f85cefef275
juliosam <ja.samaniegom@gmail.com>
2022-04-05 07:49:43 -0500

    dialog menu and first hovers

--- js.js
+++ js.js
@@ -7,8 +7,16 @@ let menubackground = document.querySelector('.page-head__menu-background-image')
 
 menubtn.addEventListener('click', function(){
 
-    console.log('cloc') ;
-    menubackground.classList.add('pressed');
-    menumobile.classList.add('pressedto');
+    if (menubackground.classList.contains('pressed')){
+         menubackground.classList.remove('pressed')
+         menumobile.classList.remove('pressedto')
+        
+        }
+    else {
+        console.log('cloc') 
+        menubackground.classList.add('pressed');
+        menumobile.classList.add('pressedto');
+        }
+
 })
 
